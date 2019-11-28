6cfda5cdf8063cfe2e680b2342c394dfed7ec49d
Anastasia Perevalova <perev4n@gmail.com>
2019-11-28 02:46:45 +0200

    feature & refactor

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from flask import request
 
 from flask_sslify import SSLify
 import time
-from .config import token
+from bot.config import token
 
 os.environ["TZ"] = "Europe/Kiev"
 time.tzset()
@@ -35,94 +35,118 @@ def parse_text(text):
     com = re.search(pattern, text).group()
     return com[1:]
 
-def timetable(message):
-    my_dir = os.path.dirname(__file__)
-    json_file_path = os.path.join(my_dir, 'schedule.json')
+# contains file with timetable
+my_dir = os.path.dirname(__file__)
+json_file_path = os.path.join(my_dir, 'schedule.json')
+
+def find_bus(sel_time, keyword):
+    """
+    Takes 2 arguments
+    :param sel_time: selected time (current time or time entered by the user)
+    :param keyword: name of list
+    :return: string with answer
+    """
     answer_string = ''
-    if 'bs1' in message:
-        with open(json_file_path, 'r') as f:
-            d = json.load(f)
-            for x in d['BS1']:
-                pr = ' - '.join(x)
-                answer_string += f'{pr}\n'
-            return answer_string
-    elif 'berezivka' in message:
-        with open(json_file_path, 'r') as f:
-            d = json.load(f)
-            for x in d['answer_string']:
-                pr = ' - '.join(x)
-                answer_string += f'{pr}\n'
-            return answer_string
-    elif 'now_br' in message:
-        with open(json_file_path, 'r') as f:
-            d = json.load(f)
-            cur_time = datetime.now().time()
-            for i in range(len(d['answer_string'])-1):
-                k = d['answer_string'][i]
-                l = d['answer_string'][i+1]
-                time_obj = datetime.strptime(k[0],
-                                                    '%H:%M').time()
-                time_obj2 = datetime.strptime(l[0],
-                                                    '%H:%M').time()
-                if time_obj < cur_time < time_obj2:
-                    pr = ' - '.join(d['answer_string'][i])
-                    nx1 = ' - '.join(d['answer_string'][i+1])
-                    nx2 = ' - '.join(d['answer_string'][i+2])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Наступний: {nx1}\n'
+    with open(json_file_path, 'r') as f:
+        d = json.load(f)
+        for i in range(len(d[keyword]) - 1):
+            sel_item = d[keyword][i]
+            next_item = d[keyword][i + 1]
+            last_bus = d[keyword][-1]
+            time_sel_item = datetime.strptime(sel_item[0],
+                                         '%H:%M').time()
+            time_next_item = datetime.strptime(next_item[0],
+                                          '%H:%M').time()
+            time_last_bus = datetime.strptime(last_bus[0],
+                                          '%H:%M').time()
+            if time_sel_item < sel_time < time_next_item:
+                pr = ' - '.join(d[keyword][i])
+                nx1 = ' - '.join(d[keyword][i + 1])
+                answer_string += f'Попередній: {pr}\n'
+                answer_string += f'Наступний: {nx1}\n'
+                try:
+                    nx2 = ' - '.join(d[keyword][i + 2])
                     answer_string += f'Наступний: {nx2}\n'
                     return answer_string
-                elif time_obj == cur_time:
-                    pr = ' - '.join(d['answer_string'][i-1])
-                    nw = ' - '.join(d['answer_string'][i])
-                    nx1 = ' - '.join(d['answer_string'][i+1])
-                    nx2 = ' - '.join(d['answer_string'][i+2])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Зараз: {nw}\n'
+                except IndexError:
+                    return answer_string
+            elif time_sel_item == sel_time:
+                pr = ' - '.join(d[keyword][i - 1])
+                nw = ' - '.join(d[keyword][i])
+                answer_string += f'Попередній: {pr}\n'
+                answer_string += f'Зараз: {nw}\n'
+                try:
+                    nx1 = ' - '.join(d[keyword][i + 1])
                     answer_string += f'Наступний: {nx1}\n'
+                    nx2 = ' - '.join(d[keyword][i + 2])
                     answer_string += f'Наступний: {nx2}\n'
                     return answer_string
-                elif cur_time < time_obj:
-                    nx1 = ' - '.join(d['answer_string'][i])
-                    nx2 = ' - '.join(d['answer_string'][i+1])
-                    answer_string += f'Наступний: {nx1}\n'
-                    answer_string += f'Наступний: {nx2}\n'
+                except IndexError:
                     return answer_string
-    else:
-        with open(json_file_path, 'r') as f:
-            d = json.load(f)
-            cur_time = datetime.strptime(message, '%H:%M').time()
-            for i in range(len(d['answer_string'])-1):
-                k = d['answer_string'][i]
-                l = d['answer_string'][i+1]
-                time_obj = datetime.strptime(k[0],
-                                                    '%H:%M').time()
-                time_obj2 = datetime.strptime(l[0],
-                                                    '%H:%M').time()
-                if time_obj < cur_time < time_obj2:
-                    pr = ' - '.join(d['answer_string'][i])
-                    nx1 = ' - '.join(d['answer_string'][i+1])
-                    nx2 = ' - '.join(d['answer_string'][i+2])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Наступний: {nx1}\n'
-                    answer_string += f'Наступний: {nx2}\n'
-                    return answer_string
-                elif time_obj == cur_time:
-                    pr = ' - '.join(d['answer_string'][i-1])
-                    nw = ' - '.join(d['answer_string'][i])
-                    nx1 = ' - '.join(d['answer_string'][i+1])
-                    nx2 = ' - '.join(d['answer_string'][i+2])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Зараз: {nw}\n'
-                    answer_string += f'Наступний: {nx1}\n'
-                    answer_string += f'Наступний: {nx2}\n'
-                    return answer_string
-                elif cur_time < time_obj:
-                    nx1 = ' - '.join(d['answer_string'][i])
-                    nx2 = ' - '.join(d['answer_string'][i+1])
-                    answer_string += f'Наступний: {nx1}\n'
-                    answer_string += f'Наступний: {nx2}\n'
-                    return answer_string
+            elif sel_time < time_sel_item:
+                nx1 = ' - '.join(d[keyword][i])
+                nx2 = ' - '.join(d[keyword][i + 1])
+                answer_string += f'Наступний: {nx1}\n'
+                answer_string += f'Наступний: {nx2}\n'
+                return answer_string
+            elif time_last_bus < sel_time:
+                return 'На сьогодні автобусів більше немає'
+
+def all_buses(keyword):
+    """
+    Take 1 argument
+    :param keyword: name of list
+    :return: List of all buses
+    """
+    answer_string = ''
+    with open(json_file_path, 'r') as f:
+        d = json.load(f)
+        for x in d[keyword]:
+            pr = ' - '.join(x)
+            answer_string += f'{pr}\n'
+        return answer_string
+
+def timetable(message):
+    bus_list = {
+        ('bs1', 'ас1'): 'busstation1',
+        ('bs2', 'ас2'): 'busstation2',
+        ('br', 'березівка'): 'berezivka',
+    }
+    bus_list1 = {
+        'ас1': 'busstation1',
+        'ас2': 'busstation2',
+        'березівка': 'berezivka',
+    }
+    bus_list2 = {
+        'bs1': 'busstation1',
+        'bs2': 'busstation2',
+        'br': 'berezivka',
+    }
+    # for current time
+    if message in  ['now_bs1', 'now_bs2', 'now_br']:
+        sel_time = datetime.now().time()
+        sel_list = message[message.find('_'):][1:]
+        for i in bus_list2:
+            keyword = bus_list2[sel_list]
+        answer = find_bus(sel_time, keyword)
+        return answer
+    # returns list of all buses
+    elif message in ['busstation1', 'busstation2', 'berezivka']:
+        answer = all_buses(message)
+        return answer
+    # possibility to set a time
+    elif 'ас1' or 'ас2' or 'березівка' in message:
+        try:
+            first, last = message.split()
+            sel_time = datetime.strptime(last, '%H:%M').time()
+            for i in bus_list1:
+                keyword = bus_list1[first]
+            answer = find_bus(sel_time, keyword)
+            return answer
+        except:
+            answer = 'Не коректно введене значення'
+            return answer
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -138,17 +162,14 @@ def index():
                 text = 'Вітаю! Цей бот призначений для орієнтації в часі пересування \
                 громадського транспорту для села Березівка.'
                 text2 = 'Доступні команди: \n' \
-                        '/now - Відправляються в найближчий час з Житомира АС2 \n' \
                         '/now_bs1 - Відправляються в найближчий час з Житомира АС1 \n' \
-                        '/now_zt - Відправляються в найближчий час з Житомира \n' \
+                        '/now_bs2 - Відправляються в найближчий час з Житомира АС2 \n' \
                         '/now_br - Відправляються в найближчий час з Березівки \n' \
-                        '/zhytomyr - Усі автобуси з Житомира \n' \
-                        '/busstasion1 - Усі автобуси з Житомира АС1 \n' \
-                        '/busstasion2 - Усі автобуси з Житомира АС2 \n' \
+                        '/busstation1 - Усі автобуси з Житомира АС1 \n' \
+                        '/busstation2 - Усі автобуси з Житомира АС2 \n' \
                         '/berezivka - Усі автобуси з Березівки \n' \
                         'Також Ви можете ввести пункт відправлення та бажаний час ' \
                         ' у форматі "години:хвилини" для того, щоб запланувати поїздку: \n' \
-                        'житомир 14:00 - Запланована поїздка з Житомира \n' \
                         'ас1 14:00 - Запланована поїздка з Житомира АС1 \n' \
                         'ас2 14:00 - Запланована поїздка з Житомира АС2 \n' \
                         'березівка 14:00 - Запланована поїздка з Березівки'
--- app/parse_message.py
+++ app/parse_message.py
@@ -21,13 +21,16 @@ def find_bus(sel_time, keyword):
     with open(json_file_path, 'r') as f:
         d = json.load(f)
         for i in range(len(d[keyword]) - 1):
-            k = d[keyword][i]
-            l = d[keyword][i + 1]
-            time_obj = datetime.strptime(k[0],
+            sel_item = d[keyword][i]
+            next_item = d[keyword][i + 1]
+            last_bus = d[keyword][-1]
+            time_sel_item = datetime.strptime(sel_item[0],
                                          '%H:%M').time()
-            time_obj2 = datetime.strptime(l[0],
+            time_next_item = datetime.strptime(next_item[0],
                                           '%H:%M').time()
-            if time_obj < sel_time < time_obj2:
+            time_last_bus = datetime.strptime(last_bus[0],
+                                          '%H:%M').time()
+            if time_sel_item < sel_time < time_next_item:
                 pr = ' - '.join(d[keyword][i])
                 nx1 = ' - '.join(d[keyword][i + 1])
                 answer_string += f'Попередній: {pr}\n'
@@ -35,10 +38,10 @@ def find_bus(sel_time, keyword):
                 try:
                     nx2 = ' - '.join(d[keyword][i + 2])
                     answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
+                    return answer_string
                 except IndexError:
-                    return print(answer_string)
-            elif time_obj == sel_time:
+                    return answer_string
+            elif time_sel_item == sel_time:
                 pr = ' - '.join(d[keyword][i - 1])
                 nw = ' - '.join(d[keyword][i])
                 answer_string += f'Попередній: {pr}\n'
@@ -48,17 +51,17 @@ def find_bus(sel_time, keyword):
                     answer_string += f'Наступний: {nx1}\n'
                     nx2 = ' - '.join(d[keyword][i + 2])
                     answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
+                    return answer_string
                 except IndexError:
                     return print(answer_string)
-            elif sel_time < time_obj:
+            elif sel_time < time_sel_item:
                 nx1 = ' - '.join(d[keyword][i])
                 nx2 = ' - '.join(d[keyword][i + 1])
                 answer_string += f'Наступний: {nx1}\n'
                 answer_string += f'Наступний: {nx2}\n'
-                return print(answer_string)
-            elif datetime.strptime('20:15', '%H:%M').time() < sel_time:
-                return print('На сьогодні автобусів більше немає')
+                return answer_string
+            elif time_last_bus < sel_time:
+                return 'На сьогодні автобусів більше немає'
 
 
 def all_buses(keyword):
@@ -69,10 +72,9 @@ def all_buses(keyword):
         for x in d[keyword]:
             pr = ' - '.join(x)
             answer_string += f'{pr}\n'
-        return print(answer_string)
+        return answer_string
 
 def timetable(message):
-    options = ['zhytomyr', 'busstasion1', 'busstasion2', 'berezivka']
     bus_list = {
         ('bs1', 'ас1'): 'busstation1',
         ('bs2', 'ас2'): 'busstation2',
@@ -92,15 +94,17 @@ def timetable(message):
         'br': 'berezivka',
     }
     # for current time
-    if 'now_bs1' or 'now_bs2' or 'now_zt'  or 'now_br' in message:
+    if message in  ['now_bs1', 'now_bs2', 'now_zt', 'now_br']:
         sel_time = datetime.now().time()
         sel_list = message[message.find('_'):][1:]
         for i in bus_list2:
             keyword = bus_list2[sel_list]
-        find_bus(sel_time, keyword)
+        answer = find_bus(sel_time, keyword)
+        return answer
     # returns list of all buses
-    elif 'zhytomyr' or 'busstasion1' or 'busstasion2' or 'berezivka' in message:
-        all_buses(message)
+    elif message in ['zhytomyr', 'busstation1', 'busstation2', 'berezivka']:
+        answer = all_buses(message)
+        return answer
     # possibility to set a time
     elif 'житомир' or 'ас1' or 'ас2' or 'березівка' in message:
         try:
@@ -108,179 +112,16 @@ def timetable(message):
             sel_time = datetime.strptime(last, '%H:%M').time()
             for i in bus_list1:
                 keyword = bus_list1[first]
-            find_bus(sel_time, keyword)
+            answer = find_bus(sel_time, keyword)
+            return answer
         except:
-            print('Error')
-
-
-def schedule(message):
-    # my_dir = os.path.dirname(__file__)
-    # json_file_path = os.path.join(my_dir, 'schedule.json')
-    json_file_path = 'schedule.json'
-    answer_string = ''
-    with open(json_file_path, 'r') as f:
-        d = json.load(f)
-    # Buses that leave now from Zhytomyr BS1
-    if 'now_bs1' in message:
-        cur_time = datetime.now().time()
-        for i in range(len(d['BS1']) - 1):
-            k = d['BS1'][i]
-            l = d['BS1'][i + 1]
-            time_obj = datetime.strptime(k[0],
-                                         '%H:%M').time()
-            time_obj2 = datetime.strptime(l[0],
-                                          '%H:%M').time()
-            if time_obj < cur_time < time_obj2:
-                pr = ' - '.join(d['BS1'][i])
-                nx1 = ' - '.join(d['BS1'][i + 1])
-                answer_string += f'Попередній: {pr}\n'
-                answer_string += f'Наступний: {nx1}\n'
-                try:
-                    nx2 = ' - '.join(d['BS1'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
-                except IndexError:
-                    return print(answer_string)
-            elif time_obj == cur_time:
-                pr = ' - '.join(d['BS1'][i - 1])
-                nw = ' - '.join(d['BS1'][i])
-                answer_string += f'Попередній: {pr}\n'
-                answer_string += f'Зараз: {nw}\n'
-                try:
-                    nx1 = ' - '.join(d['BS1'][i + 1])
-                    answer_string += f'Наступний: {nx1}\n'
-                    nx2 = ' - '.join(d['BS1'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
-                except IndexError:
-                    return print(answer_string)
-            elif cur_time < time_obj:
-                nx1 = ' - '.join(d['BS1'][i])
-                nx2 = ' - '.join(d['BS1'][i + 1])
-                answer_string += f'Наступний: {nx1}\n'
-                answer_string += f'Наступний: {nx2}\n'
-                return print(answer_string)
-            elif datetime.strptime('20:15', '%H:%M').time() < cur_time:
-                return print('На сьогодні автобусів більше немає')
-    # Buses that leave now from Berezivka
-    elif 'now_br' in message:
-        # Berezivka = ''
-        cur_time = datetime.now().time()
-        for i in range(len(d['Berezivka']) - 1):
-            k = d['Berezivka'][i]
-            l = d['Berezivka'][i + 1]
-            time_obj = datetime.strptime(k[0],
-                                         '%H:%M').time()
-            time_obj2 = datetime.strptime(l[0],
-                                          '%H:%M').time()
-            if time_obj < cur_time < time_obj2:
-                pr = ' - '.join(d['Berezivka'][i])
-                nx1 = ' - '.join(d['Berezivka'][i + 1])
-                if d['Berezivka'][i + 2]:
-                    nx2 = ' - '.join(d['Berezivka'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                answer_string += f'Попередній: {pr}\n'
-                answer_string += f'Наступний: {nx1}\n'
-                return print(answer_string)
-            elif time_obj == cur_time:
-                pr = ' - '.join(d['Berezivka'][i - 1])
-                nw = ' - '.join(d['Berezivka'][i])
-                if d['Berezivka'][i + 1]:
-                    nx1 = ' - '.join(d['Berezivka'][i + 1])
-                    answer_string += f'Наступний: {nx1}\n'
-                if d['Berezivka'][i + 2]:
-                    nx2 = ' - '.join(d['Berezivka'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                answer_string += f'Попередній: {pr}\n'
-                answer_string += f'Зараз: {nw}\n'
-                return print(answer_string)
-            elif cur_time < time_obj:
-                nx1 = ' - '.join(d['Berezivka'][i])
-                nx2 = ' - '.join(d['Berezivka'][i + 1])
-                answer_string += f'Наступний: {nx1}\n'
-                answer_string += f'Наступний: {nx2}\n'
-                return print(answer_string)
-            elif datetime.strptime('20:15', '%H:%M').time() < cur_time:
-                return print('На сьогодні автобусів більше немає')
-    # Buses that leave now from Zhytomyr BS2
-    elif 'now' in message:
-        for x in d['BS1']:
-            pr = ' - '.join(x)
-            answer_string += f'{pr}\n'
-        return print(answer_string)
-    # List of all buses from Zhytomyr BS1
-    elif 'busstation1' in message:
-        for x in d['BS1']:
-            pr = ' - '.join(x)
-            answer_string += f'{pr}\n'
-        return print(answer_string)
-    # List of all buses from Zhytomyr BS2
-    elif 'busstation2' in message:
-        for x in d['BS1']:
-            pr = ' - '.join(x)
-            answer_string += f'{pr}\n'
-        return print(answer_string)
-    # List of all buses from Zhytomyr
-    elif 'zhytomyr' in message:
-        for x in d['BS1']:
-            pr = ' - '.join(x)
-            answer_string += f'{pr}\n'
-        return print(answer_string)
-    # List of all buses from Berezivka
-    elif 'berezivka' in message:
-        for x in d['Berezivka']:
-            pr = ' - '.join(x)
-            answer_string += f'{pr}\n'
-        return print(answer_string)
-    else:
-        sel_time = datetime.strptime(message, '%H:%M').time()
-        for i in range(len(d['Berezivka']) - 1):
-            k = d['Berezivka'][i]
-            l = d['Berezivka'][i + 1]
-            time_obj = datetime.strptime(k[0],
-                                         '%H:%M').time()
-            time_obj2 = datetime.strptime(l[0],
-                                          '%H:%M').time()
-            print(time_obj == sel_time)
-            if time_obj < sel_time < time_obj2:
-                try:
-                    pr = ' - '.join(d['Berezivka'][i])
-                    nx1 = ' - '.join(d['Berezivka'][i + 1])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Наступний: {nx1}\n'
-                    nx2 = ' - '.join(d['Berezivka'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
-                except IndexError:
-                    return print(answer_string)
-                    # continue
-            elif time_obj == sel_time:
-                try:
-                    print(sel_time)
-                    pr = ' - '.join(d['Berezivka'][i - 1])
-                    nw = ' - '.join(d['Berezivka'][i])
-                    answer_string += f'Попередній: {pr}\n'
-                    answer_string += f'Зараз: {nw}\n'
-                    nx1 = ' - '.join(d['Berezivka'][i + 1])
-                    answer_string += f'Наступний: {nx1}\n'
-                    nx2 = ' - '.join(d['Berezivka'][i + 2])
-                    answer_string += f'Наступний: {nx2}\n'
-                    return print(answer_string)
-                except IndexError:
-                    return print(answer_string)
-            elif sel_time < time_obj:
-                nx1 = ' - '.join(d['Berezivka'][i])
-                nx2 = ' - '.join(d['Berezivka'][i + 1])
-                answer_string += f'Наступний: {nx1}\n'
-                answer_string += f'Наступний: {nx2}\n'
-                return print(answer_string)
-            elif datetime.strptime('20:15', '%H:%M').time() < sel_time:
-                return print('На сьогодні автобусів більше немає')
+            answer = 'Не коректно введене значення'
+            return answer
 
 
 def main():
-    all_buses('berezivka')
-    timetable('berezivka')
+    but = timetable('busstation2')
+    print(but)
 
 
 if __name__ == '__main__':
